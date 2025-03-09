b56d64a1b3ac785ecda9f2c952bf7420edd5cc3c
MarianeBelen <marianebelen15@gmail.com>
2025-03-10 02:52:25 +1200

    Add files via upload

---
+++ evenNumber.js
@@ -0,0 +1,12 @@
+/*
+Name: BELEN, Mariane I.
+Course&Section: BSCS 1B
+Activity No. Perf2
+File: evenNumber.js
+*/
+//Use foor loop to find the even numbers from 1-10.
+for (let n = 1; n <= 10; n++){  //This is the loop from 1-10
+    if (n % 2 === 0){   //Every number from 1-10 will be modulu to 2 if it's = 0 then its a even number
+        console.log(n);  //Output 2  4  6  8  10
+    }
+}
